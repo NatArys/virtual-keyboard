60ab384fcc4b346f0d7555f5936a57e3fcbc4cdb
NatArys <91931609+NatArys@users.noreply.github.com>
2023-05-03 14:44:57 +0700

    feat: add airbnb-config for linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
-module.exports = {
+{
   "env": {
     "browser": true,
     "es2021": true
   },
-  "extends": "eslint:recommended",
-  "overrides": [
+  "extends": [
+    "airbnb"
   ],
+  "overrides": [],
   "parserOptions": {
     "ecmaVersion": "latest",
     "sourceType": "module"
   },
   "rules": {
-    'import/extensions': [
-      'error', 'ignorePackages',
+    "indent": [
+        "error",
+        2
     ],
-  },
+    "linebreak-style": [
+        "error",
+        "windows"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "always"
+    ]
+}
 }
