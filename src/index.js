7e6a784c0a044278a7fdf914931ed3f00d214f7d
NatArys <91931609+NatArys@users.noreply.github.com>
2023-05-03 14:47:51 +0700

    fix: fix according to eslint-airbnb rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './index.html';
 import './index.scss';
 
-import Keyboard from './js/keyboard.js';
-import { createNewNode } from './js/createnode.js';
+import Keyboard from './js/keyboard';
+import createNewNode from './js/createnode';
 
 const body = document.querySelector('body');
 
@@ -18,10 +18,7 @@ const textWrap = createNewNode('section', '', 'input-wrapper');
 const inputField = createNewNode('textarea', '', 'input-field');
 inputField.id = 'input';
 
-
-
 window.onload = () => {
-
   body.append(textWrap);
 
   infoText.append(titleText);
@@ -34,16 +31,78 @@ window.onload = () => {
 
   body.append(keyboard.generateKeyboard());
 
+  const pressKey = (event, btn, code) => {
+    event.preventDefault();
+    inputField.focus();
+    let str = '';
 
+    // символы
+    if (btn.dataset.type === 'char' || btn.dataset.type === 'num') {
+      str = btn.textContent;
+      if (code === 'Tab') str = '    ';
+      keyboard.changeShift(event);
+      inputField.value += str;
+    }
+
+    // функциональные
+    if (btn.dataset.type === 'func') {
+      if (code === 'Enter') {
+        inputField.value += '\n';
+      }
+
+      if (code === 'CapsLock') {
+        keyboard.changeCaps(event);
+      }
+
+      if (code === 'AltLeft' && (keyboard.shift || event.shiftKey)) {
+        keyboard.сhangeLang(event);
+        keyboard.changeShift(event);
+      }
+
+      if (code === 'ShiftLeft' || code === 'ShiftRight') {
+        keyboard.replaceKeyVal(event);
+      }
+
+      if (code === 'Lang') {
+        keyboard.сhangeLang(event);
+      }
+
+      let caret = inputField.selectionStart;
+
+      if (code === 'ArrowLeft' && caret > 0) {
+        inputField.setSelectionRange(caret - 1, caret - 1);
+      }
+      if (code === 'ArrowRight') {
+        caret = inputField.selectionEnd;
+        inputField.setSelectionRange(caret + 1, caret + 1);
+      }
+      if (code === 'ArrowDown' || code === 'ArrowUp') {
+        str = btn.textContent;
+        keyboard.changeShift(event);
+        inputField.value += str;
+      }
+
+      if (code === 'Backspace') {
+        const strText = inputField.value.substring(0, caret - 1)
+          + inputField.value.substring(caret, inputField.value.length);
+        inputField.value = strText;
+        if (caret > 0) inputField.setSelectionRange(caret - 1, caret - 1);
+      }
+      if (code === 'Delete') {
+        const strText = inputField.value.substring(0, caret)
+          + inputField.value.substring(caret + 1, inputField.value.length);
+        inputField.value = strText;
+        inputField.setSelectionRange(caret, caret);
+      }
+    }
+  };
 
   // real keyboard
   // нажать
   document.addEventListener('keydown', (e) => {
-
     const button = document.querySelector(`[data-code=${e.code}]`);
     if (button) {
       button.classList.add('press');
-
       pressKey(e, button, e.code);
     }
   });
@@ -58,7 +117,6 @@ window.onload = () => {
         keyboard.changeShift(e);
         keyboard.replaceKeyVal(e);
       }
-
     }
   });
 
@@ -66,7 +124,6 @@ window.onload = () => {
   document.querySelector('.keyboard').addEventListener('click', (event) => {
     const button = event.target.closest('.key');
     if (button) {
-
       if (button.dataset.code === 'ShiftLeft' || button.dataset.code === 'ShiftRight') {
         keyboard.shift = !keyboard.shift;
         button.classList.toggle('press');
@@ -74,83 +131,4 @@ window.onload = () => {
       pressKey(event, button, button.dataset.code);
     }
   });
-
-
 };
-
-
-
-
-const pressKey = (event, btn, code) => {
-  //заморозка. фокус на инпут
-  event.preventDefault();
-  inputField.focus();
-
-  let str = '';
-
-  // символы
-  if (btn.dataset.type === 'char' || btn.dataset.type === 'num') {
-    str = btn.textContent;
-    if (code === 'Tab') str = '    ';
-    keyboard.changeShift(event);
-    inputField.value += str;
-  }
-
-
-  // функциональные
-  if (btn.dataset.type === 'func') {
-
-    if (code === 'Enter') {
-      inputField.value += '\n';
-    }
-
-    if (code === 'CapsLock') {
-      keyboard.changeCaps(event);
-    }
-
-    if (code === 'AltLeft' && (keyboard.shift || event.shiftKey)) {
-      keyboard.сhangeLang(event);
-      keyboard.changeShift(event);
-    }
-
-    if (code === 'ShiftLeft' || code === 'ShiftRight') {
-      keyboard.replaceKeyVal(event);
-    }
-
-    if (code === 'Lang') {
-      keyboard.сhangeLang(event);
-    }
-
-
-
-    let caret = inputField.selectionStart;
-
-    if (code === 'ArrowLeft' && caret > 0)
-      inputField.setSelectionRange(caret - 1, caret - 1);
-    if (code === 'ArrowRight') {
-      caret = inputField.selectionEnd;
-      inputField.setSelectionRange(caret + 1, caret + 1);
-    }
-    if (code === 'ArrowDown' || code === 'ArrowUp') {
-      str = btn.textContent;
-      keyboard.changeShift(event);
-      inputField.value += str;
-    }
-
-
-    if (code === 'Backspace') {
-      let str = inputField.value.substring(0, caret - 1) + inputField.value.substring(caret, inputField.value.length);
-      inputField.value = str;
-      if (caret > 0) inputField.setSelectionRange(caret - 1, caret - 1);
-    }
-    if (code === 'Delete') {
-      let str = inputField.value.substring(0, caret) + inputField.value.substring(caret + 1, inputField.value.length);
-      inputField.value = str;
-      inputField.setSelectionRange(caret, caret);
-    }
-
-
-  }
-
-}
-
